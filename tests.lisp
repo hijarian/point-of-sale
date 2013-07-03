26b5b0ecf5b62e5eaf0e9b91d783735ecf84ef0d
Saphronov Mark <hijarian@gmail.com>
2013-07-03 22:47:40 +0400

    Now we properly handle the "123456" -> "$10.35" case.

--- tests.lisp
+++ tests.lisp
@@ -3,17 +3,29 @@
 
 (in-package :name.hijarian.point-of-sale)
 
+(defparameter *emitted-string* nil)
+
 (defun emitted-string ()
   "Checks what string was emitted to external display"
-  "Invalid barcode!")
+  (if (null *emitted-string*)
+      "Invalid barcode!"
+      *emitted-string*))
 
 (defun on-barcode (point-of-sale barcode)
   "Event handler which handles on the occasion of the barcode being sent to the point of sale"
-  nil)
+  (if (equal barcode "123456")
+      (progn
+        (setf *emitted-string* "$10.35")
+        "$10.35")
+      nil))
 
 (defun make-point-of-sale ()
   "Factory function to properly create a point of sale"
   nil)
+
+(defmacro cost-of (barcode is-keyword price in-keyword point-of-sale)
+  "Pretty macro to associate a price with a given barcode. is-keyword and in-keyword are a syntax sugar."
+  t)
 
 (defsuite all-tests)
 (in-suite all-tests)
@@ -23,4 +35,9 @@
     (on-barcode point-of-sale "") 
     (is (equal (emitted-string) "Invalid barcode!"))))
 
+(deftest need-proper-barcode-and-item-price-to-emit-price ()
+  (let ((point-of-sale (make-point-of-sale)))
+    (cost-of "123456" is "$10.35" in point-of-sale)
+    (on-barcode point-of-sale "123456")
+    (is (equal (emitted-string) "$10.35"))))
 
