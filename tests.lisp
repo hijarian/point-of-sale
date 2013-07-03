60e26992826a6ed9df7897cd2a6ae297fded9ea3
Saphronov Mark <hijarian@gmail.com>
2013-07-03 22:04:28 +0400

    Now we have working test suite.

---
+++ tests.lisp
@@ -0,0 +1,12 @@
+(defpackage :name.hijarian.point-of-sale
+  (:use :common-lisp :stefil))
+
+(in-package :name.hijarian.point-of-sale)
+
+(defsuite all-tests)
+
+(in-suite all-tests)
+
+(deftest nothing ()
+  t)
+
