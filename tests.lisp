f541cf6979c3c24fef49af71f33bc3b2911f8b59
Saphronov Mark <hijarian@gmail.com>
2013-07-03 22:12:17 +0400

    Now we report invalid barcode in the case of empty input string

--- tests.lisp
+++ tests.lisp
@@ -3,10 +3,24 @@
 
 (in-package :name.hijarian.point-of-sale)
 
-(defsuite all-tests)
+(defun emitted-string ()
+  "Checks what string was emitted to external display"
+  "Invalid barcode!")
 
+(defun on-barcode (point-of-sale barcode)
+  "Event handler which handles on the occasion of the barcode being sent to the point of sale"
+  nil)
+
+(defun make-point-of-sale ()
+  "Factory function to properly create a point of sale"
+  nil)
+
+(defsuite all-tests)
 (in-suite all-tests)
 
-(deftest nothing ()
-  t)
+(deftest empty-string-is-invalid ()
+  (let ((point-of-sale (make-point-of-sale)))
+    (on-barcode point-of-sale "") 
+    (is (equal (emitted-string) "Invalid barcode!"))))
+
 
